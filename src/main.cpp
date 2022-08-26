553718cd36c3042e468beeb4568d0b5f4c93829f
Fabio Zanini <fabio.zanini@fastmail.fm>
2022-08-27 06:24:43 +1000

    First tests compiling

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <pybind11/pybind11.h>
+#include "treasuremap.c"
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+int add(int i, int j) {
+    return i + j;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(treasuremap, m) {
+    m.doc() = R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+        .. currentmodule:: treasuremap
+        .. autosummary::
+           :toctree: _generate
+           add
+           subtract
+    )pbdoc";
+
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+        Some other explanation about the subtract function.
+    )pbdoc");
+
+    m.def("treasuremap_2d", &treasuremap_2d, R"pbdoc(
+        TREASUREMAP in 2 dimensions.
+    )pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
